cc0966c05bd19ef37c01c492e8aa6c768e830c3a
Kartikay-101 <k.matrix.hacked@gmail.com>
2023-10-16 07:29:16 +0530

    Initial commit

---
+++ ATM.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+    void main()
+    {
+        int pass, amt;
+        printf("\n\n\t\t\t****INSERT THE CARD HERE****\n\n");
+        printf("\t\tENTER PIN =   ");
+        scanf("%d",&pass);
+        if (pass==151997)
+            printf("\n\t WITHDRWAL AMOUNT :-   ");
+            scanf("%d",&amt);
+            if (amt<=50000)
+            {
+                printf("\n\t\t WITHDRWAL TRANSACTION ~~  ''%d''",amt);
+            }
+            else
+                printf("\n\t\tGAREEBOOOO\n\t\t\t YOUR LIMIT HAS BEEN EXCEED\n");
+        }
+        else
+            printf("\n\n\t\tWrong Password");
