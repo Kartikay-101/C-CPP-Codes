8b474858c04ae801667f2566d4ba17bae95b9946
kartikay-101 <k.matrix.hacked@gmail.com>
2024-03-18 18:38:13 +0530

    kk

--- prgram of atm.c
+++ prgram of atm.c
@@ -20,11 +20,7 @@ int main() {
         case '-':
             printf("Result: %d\n", num1 - num2);
             break;
-        case '*':
-            printf("Result: %d\n", num1 * num2);
-            break;
-        case '/':
-            if (num2 != 0) {
+       
                 printf("Result: %.2f\n", (float) num1 / num2);
             } else {
                 printf("Error: Division by zero is not allowed.\n");
