97ac9ce1e550b804e65b322d6e5b444738117fda
kartikay-101 <k.matrix.hacked@gmail.com>
2024-03-18 18:45:34 +0530

    d

--- C++/Factorial.cpp
+++ C++/Factorial.cpp
@@ -11,17 +11,17 @@ long long factorial(int n) {
     }
 }
 
-int main() {
-    int number;
-    cout << "Enter a positive integer: ";
-    cin >> number;
+// int main() {
+//     int number;
+//     cout << "Enter a positive integer: ";
+//     cin >> number;
 
-    if(number < 0) {
-        cout << "Factorial of negative numbers is not defined." << endl;
-    }
-    else {
-        cout << "Factorial of " << number << " = " << factorial(number) << endl;
-    }
+//     if(number < 0) {
+//         cout << "Factorial of negative numbers is not defined." << endl;
+//     }
+//     else {
+//         cout << "Factorial of " << number << " = " << factorial(number) << endl;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
